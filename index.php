4f87e4ca4fcbbec6c2db3e7326100797bffb44da
Serafim1979 <korolkov.ivan@mail.ru>
2020-06-12 07:06:08 +0400

    Start backend

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<?php
+    //include header.php file
+    include ("header.php");
+?>
+
+<?php
+    /* include banner-area.php */
+    include ('Template/_banner-area.php');
+
+    /* include top-sale.php*/
+    include ('Template/_top-sale.php');
+
+    /* include special-price*/
+    include ('Template/_special-price.php');
+
+    /* include banner-ads.php */
+    include ('Template/_banner-ads.php');
+
+    /* include new-phones.php */
+    include ('Template/_new-phones.php');
+
+    /* include blogs.php */
+    include ('Template/_blogs.php');
+?>
+
+<?php
+//include footer.php file
+include ("footer.php");
+?>
+
